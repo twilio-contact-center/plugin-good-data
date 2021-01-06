a6448284fff28bedc392bd0f278fe87bdf586b3c
user.name <dskryl@twilio.com>
2021-01-06 10:30:55 +0100

    updated handleIncomingMessage

--- src/components/DashboardSelector/HandleUserActionEvent.jsx
+++ src/components/DashboardSelector/HandleUserActionEvent.jsx
@@ -30,22 +30,17 @@ export class HandleUserActionEvent extends React.PureComponent {
         }
         // Listen to GoodData events
         try {
-            const { name, data, eventId } = message.gdc.event;
-            this.handler_uiLink(name, data, eventId);
-        } catch (e) {
-            try {
-                const { name, data } = message.gdc;
+            const { name, data } = message.gdc;
         // Capture events related when a users clicks a link - external window
         // https://help.gooddata.com/doc/en/building-on-gooddata-platform/gooddata-integration-into-your-application/embed-gooddata-elements-into-your-applications/embed-dashboards-and-reports/embedded-dashboard-and-report-api-events-and-methods/embedded-dashboard-events#EmbeddedDashboardEvents-Auserclicksalink-externalwindow
-                if (name === 'ui.link') { 
-                    //get segment id from the URI
-                    const segment = data.uri.substring(data.uri.lastIndexOf('/') + 1);
-                                        
-                    console.log("catching Flex Insights click event", data, name, segment);
-                    this.handler_uiLink(segment);}
-                } catch (ex) {
-                    console.log("HandleUserActionEvent: Unable to parse event.name and event.data");
-            }
+            if (name === 'ui.link') { 
+                //get segment id from the URI
+                const segment = data.uri.substring(data.uri.lastIndexOf('/') + 1);
+                                    
+                console.log("catching Flex Insights click event", data, name, segment);
+                this.handler_uiLink(segment);}
+            } catch (ex) {
+                console.log("HandleUserActionEvent: Not a ui.link event");
         }
     };
 
